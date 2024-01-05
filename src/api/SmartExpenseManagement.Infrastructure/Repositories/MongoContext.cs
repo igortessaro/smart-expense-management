dff8cae735109c93b4d97dc5b1cafdf083181372
Igor Tessaro <igorstessaro57@gmail.com>
2024-01-05 11:28:36 -0300

    chore(MongoDB): add validation on database settings

--- src/api/SmartExpenseManagement.Infrastructure/Repositories/MongoContext.cs
+++ src/api/SmartExpenseManagement.Infrastructure/Repositories/MongoContext.cs
@@ -9,9 +9,20 @@ public sealed class MongoContext
     private readonly MongoClient _client;
     private readonly IMongoDatabase _database;
 
-    public MongoContext(IOptions<DatabaseSettings> dbOptions)
+    public MongoContext(IOptionsMonitor<DatabaseSettings> dbOptions)
     {
-        var settings = dbOptions.Value;
+        var settings = dbOptions.CurrentValue;
+
+        if (string.IsNullOrEmpty(settings.ConnectionString))
+        {
+            throw new ArgumentNullException(nameof(settings.ConnectionString));
+        }
+
+        if (string.IsNullOrEmpty(settings.DatabaseName))
+        {
+            throw new ArgumentNullException(nameof(settings.DatabaseName));
+        }
+
         _client = new MongoClient(settings.ConnectionString);
         _database = _client.GetDatabase(settings.DatabaseName);
     }
